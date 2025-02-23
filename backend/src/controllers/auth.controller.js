412c6d2847e768f6f62583271ed9120ff8751755
Tommy Oliver <oliver.tommy@icloud.com>
2025-02-23 17:57:51 +0000

    Add checkAuth functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,3 +113,13 @@ export const updateProfile = async(req, res) => {
     }
 };
 
+// Check authentication functionality
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
